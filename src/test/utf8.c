03614bc4b3ef730a7fd066d053995ac1fb4b612b
Paul Boyd <boyd.paul2@gmail.com>
2020-06-27 15:21:05 -0400

    UTF-8 decode function

--- src/test/utf8.c
+++ src/test/utf8.c
@@ -11,7 +11,7 @@ void test_utf8_decode(CuTest *tc) {
 
     copy_string(buf, "", 1);
     actual = utf8_decode(buf, &size);
-    CuAssertIntEquals(tc, 0xfffd, actual);
+    CuAssertIntEquals(tc, 0, actual);
     CuAssertIntEquals(tc, 1, size);
 
     copy_string(buf, "\xe2", 2);
@@ -40,17 +40,41 @@ void test_utf8_decode(CuTest *tc) {
     CuAssertIntEquals(tc, 4, size);
 }
 
+void test_utf8_decode_string(CuTest *tc) {
+    rune actual[13];
+    rune expected[13] = {'s', 'o', 'm', 'e', ' ', 's', 't', 'r', 'i', 'n', 'g', 0x1F387, 0};
+
+    byte string[] = {"some string\xf0\x9f\x8e\x87"};
+    byte *p = &string[0];
+
+    size_t size;
+    for (int i = 0; ; i++) {
+        actual[i] = utf8_decode(p, &size);
+        p += size;
+
+        if (actual[i] == 0) {
+            break;
+        }
+    }
+
+    for (int i = 0; i < 13; i++) {
+        CuAssertIntEquals(tc, expected[i], actual[i]);
+    }
+}
+
 void test_utf8_len(CuTest *tc) {
     CuAssertIntEquals(tc, 0, utf8_len('\x80'));
     CuAssertIntEquals(tc, 1, utf8_len('\x61'));
     CuAssertIntEquals(tc, 2, utf8_len('\xc6'));
     CuAssertIntEquals(tc, 3, utf8_len('\xe2'));
     CuAssertIntEquals(tc, 4, utf8_len('\xf0'));
+    CuAssertIntEquals(tc, 0, utf8_len('\xf8'));
 }
 
 CuSuite* utf8_suite() {
     CuSuite* suite = CuSuiteNew();
     SUITE_ADD_TEST(suite, test_utf8_decode);
+    SUITE_ADD_TEST(suite, test_utf8_decode_string);
     SUITE_ADD_TEST(suite, test_utf8_len);
     return suite;
 }
--- src/utf8.c
+++ src/utf8.c
@@ -1,16 +1,18 @@
 #include <ustring.h>
 
+#define UNICODE_REPLACEMENT 0xFFFD
+
 rune utf8_decode(byte *buf, size_t *size) {
     rune code;
     int i;
     byte b;
 
-    *size = utf8_len(*buf);
-    if (*size == 0) {
+    if (*buf == 0) {
         *size = 1;
-        return 0xfffd;
+        return 0;
     }
 
+    *size = utf8_len(*buf);
     if (*size == 1) {
         return (rune)*buf;
     }
@@ -23,14 +25,14 @@ rune utf8_decode(byte *buf, size_t *size) {
         // Make sure the two high bits are 1 and 0 respectively.
         if ((b>>6) != 2) {
             *size = i+1;
-            return 0xfffd;
+            return UNICODE_REPLACEMENT;
         }
 
         // There are 6 bits of the code point in this byte. So shift
         // everything to make room.
         code <<= 6;
 
-        // Put the low 6 bits of b on the low 6 bits of char.
+        // Copy the lower six bits of the byte.
         code |= (b & 0xff >> 2);
     }
 
@@ -38,27 +40,28 @@ rune utf8_decode(byte *buf, size_t *size) {
 }
 
 size_t utf8_len(byte b) {
-    if (b == 0) {
-        return 0;
-    }
-
-    // Find the first 0 bit
+    // The first 0 bit indicates the length:
     // 
     // 0b0xxxxxxx is 1 byte
     // 0b10xxxxxx is invalid
     // 0b110xxxxx is 2 bytes
     // 0b1110xxxx is 3 bytes
     // 0b11110xxx is 4 bytes
-    for (size_t i = 0; i <= 4; i++) {
-        if ((b&0x80>>i) == 0) {
-            switch (i) {
-            case 0:
-                return 1;
-            case 1:
-                return 0;
-            default:
-                return i;
-            }
+    byte mask = 0x80;
+    if ((b&mask) == 0) {
+        return 1;
+    }
+
+    mask >>= 1;
+    if ((b&mask) == 0) {
+        // The byte is 0b10xxxxxx, which is invalid for the first byte.
+        return 0;
+    }
+
+    for (size_t i = 2; i <= 4; i++) {
+        mask >>= 1;
+        if ((b&mask) == 0) {
+            return i;
         }
     }
 
